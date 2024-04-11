8093dd7fc0891835bae1d5cb85042bcb0a9752a2
Mehdi Faraji <mahdifarajideveloper@gmail.com>
2024-04-12 00:43:41 +0330

    added tailwindcss to project

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import { FC } from "react";
 interface AppProps {}
 
 const App: FC<AppProps> = () => {
-  return <div></div>;
+  return (
+    <h1 className="text-3xl font-bold text-red-600 underline">Hello world!</h1>
+  );
 };
 
 export default App;
