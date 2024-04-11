1ae5eed8c18a51636a803fc48cce8ac6be9fc8b9
Mehdi Faraji <mahdifarajideveloper@gmail.com>
2024-04-12 00:53:11 +0330

    added fonts and tailwind config for fonts, added primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#790069",
+      },
+      fontFamily: {
+        iranyekan: ["iranyekan", "-apple-system"],
+      },
+    },
   },
   plugins: [],
 };
